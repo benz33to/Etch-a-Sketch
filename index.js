ef7c3b3d24a8d0f07e3c139c880d8e33f7db3001
benz33to <breoferbur@gmail.com>
2023-03-22 19:43:04 +0100

    Generate random cell color

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 const sketchGrid = document.querySelector('.sketch-grid');
 const resetButton = document.querySelector('#reset-button');
 const squareNumberButton = document.querySelector('#square-number-button');
+let squareColor = getRandomColor();
+
+function getRandomColor() {
+    const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+    const r = randomBetween(0, 255);
+    const g = randomBetween(0, 255);
+    const b = randomBetween(0, 255);
+    return `rgb(${r},${g},${b})`;
+}
 
 function draw(event) {
     if (!event.target.classList.contains('square')) return; // Do nothing if target event is not a square
     const square = event.target;
-    square.classList.add('colored');
+    square.style.backgroundColor = squareColor;
     sketchGrid.addEventListener('mouseover', draw);
 };
 
 function endDraw() {
+    squareColor = getRandomColor();
     sketchGrid.removeEventListener('mouseover', draw);
 };
 
 function resetDrawing() {
     Array.from(sketchGrid.children).forEach(square => {
-        square.classList.remove('colored');
+        square.style.backgroundColor = 'white';
     })
 };
 
@@ -39,10 +49,11 @@ function buildGrid (cells = 16) {
     for (let rowCell = 0; rowCell < cells; rowCell++) {
         for (let colCell = 0; colCell < cells; colCell++) {
             const newDiv = document.createElement('div');
+            newDiv.addEventListener('mousedown', (e) => e.preventDefault()); // Prevents drag pointer to appear
             newDiv.classList.add('square');
             sketchGrid.appendChild(newDiv);
             sketchGrid.addEventListener('mousedown', draw);
-            sketchGrid.addEventListener('mouseup', endDraw)
+            sketchGrid.addEventListener('mouseup', endDraw);
         };
     };
 }
